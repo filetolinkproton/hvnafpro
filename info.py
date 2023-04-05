38b4c205d9d99382657caa637d53c3edd01bd811
filetolinkproton <121384896+filetolinkproton@users.noreply.github.com>
2023-04-05 17:27:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '7433052') )
 API_HASH = environ.get('API_HASH', '1b2bccffd1fd3570a39a8bcf7581f28b')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6060892676:AAHFyY_ahrvGI0Hx36d6kh7OINCESgOvqRs')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5748081854:AAGxRBsqJBuds8RnSyHsS8cPf2uKYQug6yk')
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
